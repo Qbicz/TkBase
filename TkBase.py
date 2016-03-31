5433a44bb4515aff7bc6d2b68df7d58121e6dba6
Qbicz <avenger.v14@gmail.com>
2016-03-31 21:21:01 +0200

    remove excess widgets

--- TkBase.py
+++ TkBase.py
@@ -178,11 +178,23 @@ def writeDeleteRecord():
 def closePopup(popup):
     popup.destroy()
   
+def switchAdminMode(newMode):
+    print('switchAdminMode()')
+    
+    if newMode == Mode
+        pass
+    elif newMode == 'devel'
+        pass
+    elif newMode == 'admin'
+        pass
+    elif newMode == 'user'
+        print('> user mode')
+        
+  
 def showRowsFromTable(*args):
     print('showRowsFromTable()')
     dbRows = [] # istnieje tylko wewnatrz funkcji
     updateSelectedTable()
-    
     
     # optional args - leave it here as an example of event.widget
     """
@@ -233,6 +245,10 @@ def showPopulation(*args):
         statusmsg.set("The population of %s (%s) is %d" % (name, code, popn))
     sentmsg.set('')
 
+def showRowInfo():
+    print('showRowInfo')
+    # here put info about current record
+    
 # Called when the user double clicks an item in the listbox, presses
 # the "Send Gift" button, or presses the Return key.  In case the selected
 # item is scrolled out of view, make sure it is visible.
@@ -278,32 +294,9 @@ dbRows = []
     
 # ----- Tkinter part -------
 
-# Initialize our country "databases":
-#  - the list of country codes (a subset anyway)
-#  - a parallel list of country names, in the same order as the country codes
-#  - a hash table mapping country code to population<
-countrycodes = ('ar', 'au', 'be', 'br', 'ca', 'cn', 'dk', 'fi', 'fr', 'gr', 'in', 'it', 'jp', 'mx', 'nl', 'no', 'es', 'se', 'ch')
-countrynames = ('Argentina', 'Australia', 'Belgium', 'Brazil', 'Canada', 'China', 'Denmark', \
-        'Finland', 'France', 'Greece', 'India', 'Italy', 'Japan', 'Mexico', 'Netherlands', 'Norway', 'Spain', \
-        'Sweden', 'Switzerland')
-#cnames = StringVar(value=countrynames)
-
-populations = {'ar':41000000, 'au':21179211, 'be':10584534, 'br':185971537, \
-        'ca':33148682, 'cn':1323128240, 'dk':5457415, 'fi':5302000, 'fr':64102140, 'gr':11147000, \
-        'in':1131043000, 'it':59206382, 'jp':127718000, 'mx':106535000, 'nl':16402414, \
-        'no':4738085, 'es':45116894, 'se':9174082, 'ch':7508700}
-
-# Names of the gifts we can send
-gifts = { 'card':'Greeting card', 'flowers':'Flowers', 'nastygram':'Nastygram'}
-
 # State variables
-gift = StringVar()
-sentmsg = StringVar()
-statusmsg = StringVar()
 searchText = StringVar()
-
 tablenames = StringVar(value=chooseTable)
-
         
 # Create and grid the outer content frame
 c = ttk.Frame(root, padding=(5, 5, 12, 0))
@@ -313,18 +306,10 @@ root.grid_rowconfigure(0,weight=1)
 
 # Create the different widgets; note the variables that many
 # of them are bound to, as well as the button callback.
-# Note we're using the StringVar() 'cnames', constructed from 'countrynames'
 tablelabel = ttk.Label(c, text='Tabele')
 tablebox = Listbox(c, listvariable=tablenames)
 rowlabel = ttk.Label(c, text='Wyniki')
 lbox = Listbox(c, listvariable=cnames, height=5)
-lbl = ttk.Label(c, text="Send to country's leader:")
-g1 = ttk.Radiobutton(c, text=gifts['card'], variable=gift, value='card')
-g2 = ttk.Radiobutton(c, text=gifts['flowers'], variable=gift, value='flowers')
-g3 = ttk.Radiobutton(c, text=gifts['nastygram'], variable=gift, value='nastygram')
-send = ttk.Button(c, text='Send Gift', command=sendGift, default='active')
-sentlbl = ttk.Label(c, textvariable=sentmsg, anchor='center')
-status = ttk.Label(c, textvariable=statusmsg, anchor=W)
 searchLabel = ttk.Label(c, text='Wyszukaj w tablicy')
 searchbar = ttk.Entry(c, textvariable = searchText)
 searchText.set("np. Gibson")
@@ -332,47 +317,37 @@ b = ttk.Button(c, text="Szukaj", width=10, command=getSearchText)
 newButton = ttk.Button(c, text="Dodaj nowy", command=newRecord, default='active')
 updateButton = ttk.Button(c, text="Modyfikuj", command=updateRecord, default='active')
 deleteButton = ttk.Button(c, text="Usuń", command=deleteRecord, default='active')
+adminButton = ttk.Button(c, text="Tryb administratora", command=switchAdminMode, default='active')
 
 # Grid all the widgets
 tablelabel.grid(column=0,row=0,pady=5)
-tablebox.grid(column=0,row=1, rowspan=5, padx=10, sticky=(N,S,E,W))
-lbox.grid(column=1, row=1, rowspan=5, columnspan=4, sticky=(N,S,E,W)) # add a widget to column 2
+tablebox.grid(column=0,row=1, rowspan=6, padx=10, sticky=(N,S,E,W))
+lbox.grid(column=1, row=1, rowspan=6, columnspan=4, sticky=(N,S,E,W)) # add a widget to column 2
 rowlabel.grid(column=1,row=0,pady=5)
-lbl.grid(column=5, row=0, padx=10, pady=5)
-g1.grid(column=5, row=1, sticky=W, padx=20)
-g2.grid(column=5, row=2, sticky=W, padx=20)
-g3.grid(column=5, row=3, sticky=W, padx=20)
-send.grid(column=5, row=6, sticky=(W,E))
-sentlbl.grid(column=5, row=5, columnspan=2, sticky=N, pady=5, padx=5)
-status.grid(column=4, row=6, columnspan=2, sticky=(W,E))
 searchLabel.grid(column=6, row=0, sticky=(N,S,E,W))
 searchbar.grid(column=6, row=1, sticky=W)
 newButton.grid(column=6, row=4, padx=5, pady=5)
 updateButton.grid(column=6, row=5, padx=5, pady=5)
 deleteButton.grid(column=6, row=6, padx=5, pady=5)
+adminButton.grid(column=2, row=7)
 
+
+lbox
 b.grid(column=5, row=1, sticky=W)
 c.grid_columnconfigure(0, weight=1)
 c.grid_rowconfigure(5, weight=1)
 
 # Set event bindings for when the selection in the listbox changes,
 # when the user double clicks the list, and when they hit the Return key
-lbox.bind('<<ListboxSelect>>', showPopulation)
-lbox.bind('<Double-1>', sendGift)
-root.bind('<Return>', sendGift)
+lbox.bind('<<ListboxSelect>>', showRowInfo)
+#lbox.bind('<Double-1>', sendGift)
+#root.bind('<Return>', sendGift)
 tablebox.bind('<<ListboxSelect>>', showRowsFromTable)
 
 # Colorize alternating lines of the listbox
 for i in range(0,len(dbRows),2):
     lbox.itemconfigure(i, background='#f0f0ff')
 
-# Set the starting state of the interface, including selecting the
-# default gift to send, and clearing the messages.  Select the first
-# country in the list; because the <<ListboxSelect>> event is only
-# generated when the user makes a change, we explicitly call showPopulation.
-gift.set('card')
-sentmsg.set('')
-statusmsg.set('')
 lbox.selection_set(0)
 tablebox.selection_set(0)
 # global var gTable
@@ -383,8 +358,7 @@ gTable = getSelectedTable()
 # on event
 #searchText = searchbar.get('1.0', 'end')
 
-showPopulation()
-#showRowsFromTable()
+showRowsFromTable()
 
 root.wm_title('TkBase - Zarządzaj swoją bazą danych')
 root.mainloop()
